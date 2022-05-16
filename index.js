69bbcaf23ed7e690bda7963f38cce5b5a5dc5cad
namanvinod <namanvinod1@gmail.com>
2022-05-16 12:11:34 +0530

    Added

--- index.js
+++ index.js
@@ -1,3 +1,79 @@
-// run `node index.js` in the terminal
+// const possibleValues = ['X', 'G'];
+// let doors = ['X', 'X', 'X'];
+let doorWithPrize = -1;
+let playerChoice = -1;
+let revealedDoorIndex = -1;
+let totalWon = -1;
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const setup = () => {
+  doorWithPrize = Math.floor(3 * Math.random().toFixed(2));
+  // for (let i = 0; i < 10; i++) {
+  // const randomNum = Math.floor(3 * Math.random().toFixed(2));
+  // doors[randomNum] = 'C';
+  // console.log(randomNum, Math.ceil(3 * randomNum));
+  // }
+};
+
+const choose = () => {
+  playerChoice = Math.floor(3 * Math.random().toFixed(2));
+};
+
+const revealDoor = () => {
+  do {
+    revealedDoorIndex = Math.floor(3 * Math.random().toFixed(2));
+  } while (
+    playerChoice === revealedDoorIndex ||
+    revealedDoorIndex === doorWithPrize
+  );
+};
+
+const switchChoice = () => {
+  let newPlayerChoice = [0, 1, 2].filter(
+    (item) => ![revealedDoorIndex, playerChoice].includes(item)
+  );
+
+  // console.log(
+  //   `doorWithPrize: ${doorWithPrize}, playerChoice: ${playerChoice}, revealedDoorIndex: ${revealedDoorIndex}, newPlayerChoice: ${newPlayerChoice[0]}`
+  // );
+  // doorWithPrize,
+  // playerChoice,
+  // revealedDoorIndex,
+  // newPlayerChoice[0]
+  // );
+
+  playerChoice = newPlayerChoice[0];
+};
+
+const isPlayerWinner = () => {
+  // console.log(doors, playerChoice, revealedDoorIndex);
+  return doorWithPrize === playerChoice;
+};
+
+const play = (doesPlayerSwitch = true) => {
+  setup();
+
+  choose();
+
+  revealDoor();
+
+  if (doesPlayerSwitch) {
+    switchChoice();
+  }
+
+  return isPlayerWinner();
+};
+
+const totalGamesToBePlayed = 1000;
+
+for (let i = 1; i <= totalGamesToBePlayed; i++) {
+  const won = play(false);
+  if (won) totalWon++;
+  // console.log(`Game ${i}: ${won ? 'Won' : 'Lost'}`);
+}
+
+console.log(
+  `Total Games: ${totalGamesToBePlayed}, Total Games Won: ${totalWon}, Win Percentage: ${(
+    (totalWon * 100) /
+    totalGamesToBePlayed
+  ).toFixed(2)}`
+);
